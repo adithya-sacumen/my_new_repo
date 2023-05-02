c1139bfa3636e6123475323999010b111da09cf8
Sourabh Kamal <sourabh.k@sacumen.com>
2023-05-02 10:21:27 +0530

    armstrong module is implemented

---
+++ armstrong.py
@@ -0,0 +1,11 @@
+#defining a function to find a given number is armstrong or not
+def armstrong(n):
+    copy, sum = n, 0
+    while copy > 0:
+        rem = copy%10
+        sum += rem**(len(str(n)))
+        copy//=10
+    return f'{n} is an Armstrong number.' if sum == n else f'{n} is not an Armstrong number.'
+
+if __name__ == '__main__':
+    print(armstrong(13))
